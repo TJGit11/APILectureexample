23dcd449a34903666347b6177f23bf3b79e4eb1e
TJGit11 <74613952+TJGit11@users.noreply.github.com>
2021-03-01 10:46:43 -0600

    first commit

---
+++ src/main/java/com/tts/apilecture/repo/EngineRepository.java
@@ -0,0 +1,11 @@
+package com.tts.apilecture.repo;
+
+
+
+import com.tts.apilecture.model.Engine;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EngineRepository extends CrudRepository<Engine, Long> {
+}
